ad9559fb014b8ba77f9a32e133d3afc593ca25f3
livingfree2023 <livingfree2023@outlook.com>
2024-06-01 09:07:30 +0800

    change function name, echo selected ip to archive file

--- script/cfst_ddns.sh
+++ script/cfst_ddns.sh
@@ -8,7 +8,7 @@ export PATH
 #	项目: https://github.com/XIU2/CloudflareSpeedTest
 # --------------------------------------------------------------
 
-_READ() {
+read_config() {
   source cfst_ddns.conf
 }
 
@@ -39,7 +39,7 @@ notify_tg()
 }
 
 
-_TESTCURRENT()
+test_current()
 {
   
   CURRENTIP=$(nslookup $NAME 1.1.1.1 | \
@@ -58,7 +58,7 @@ _TESTCURRENT()
   echo "  Current Speed: $CURRENTSPEED MB/s" 
 }
 
-_UPDATE() 
+test_and_update() 
 {
 
   # 这里可以自己添加、修改 CloudflareST 的运行参数
@@ -76,14 +76,14 @@ _UPDATE()
       exit 0
   fi
   NEWSPEED=$(cat NEWSPEED.tmp |awk -F',' 'NR==2 {print $6}')
-  
+  echo $NEWSPEED >> result_archive.txt
   notify_tg "  优选成功，准备更新$CONTENT@$NEWSPEED to $NAME"
   DDNS_RESULT=$(timeout 20s curl -X PUT "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records/${DNS_RECORDS_ID}" \
       -H "X-Auth-Email: ${EMAIL}" \
       -H "X-Auth-Key: ${KEY}" \
       -H "Content-Type: application/json" \
       --data "{\"type\":\"${TYPE}\",\"name\":\"${NAME}\",\"content\":\"${CONTENT}\",\"ttl\":${TTL},\"proxied\":${PROXIED}}" )
-  if [ $? == 124 ];then
+  if [ $? == 124 ]; then
     echo '  DDNS请求超时,请检查网络是否重启完成并是否能够访问api.cloudflare.com'
     exit 1
   fi
@@ -107,11 +107,11 @@ handle_exit(){
     uci commit
       echo "  TCP Proxy Mode = $last_tcp_mode"
   fi
-  date "+*** Goodbye %m/%d %H:%M"
+  date "+<<<< Goodbye %m/%d %H:%M <<<<"
 }
 
 main(){
-  date "+*** Hello %m/%d %H:%M"
+  date "+>>>> Hello   %m/%d %H:%M >>>>"
   trap handle_exit EXIT HUP INT TERM
   last_tcp_mode=$(uci get "passwall.@global[0].tcp_proxy_mode")
   echo "  TCP Proxy Mode = $last_tcp_mode"
@@ -122,12 +122,12 @@ main(){
     echo "  TCP Proxy Mode = Disabled"
   fi
 
-  _READ
+  read_config
   #cd "${FOLDER}"
-  _TESTCURRENT
+  test_current
   if [[ $(echo "$CURRENTSPEED - $TARGETSPEED" | bc) < 0  ]]; then
     notify_tg "  Current $CURRENTIP @ $CURRENTSPEED MB/s < Target $TARGETSPEED MB/s, selecting new IP"
-    _UPDATE
+    test_and_update
   else
     echo "  Current $CURRENTIP @ $CURRENTSPEED > Target $TARGETSPEED, abort"
   fi
